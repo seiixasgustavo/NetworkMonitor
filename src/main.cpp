72e449fbc4b8a6afbb01dc95f65d054fe0a85618
Gustavo Nabak Batista Seixas <gunabakbs@gmail.com>
2021-02-28 19:09:51 -0300

    Adding vim configuration for YCM and Log function

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,24 @@
 
 #include <iostream>
 
+using tcp = boost::asio::ip::tcp;
+
+void Log(boost::system::error_code ec) {
+	std::cerr << (ec ? "Error: " : "OK")
+	  	  << (ec ? ec.message() : "")
+		  << std::endl;
+}
+
 int main() {
+	boost::asio::io_context ioc{};
+
+	tcp::socket socket{ioc};
+
 	boost::system::error_code ec {};
+
+	auto address {boost::asio::ip::address::from_string("1.1.1.1")};
+	tcp::endpoint endpoint{address, 80};
+	socket.connect(endpoint, ec);
 	if (ec) {
 		std::cerr << "Error: " << ec.message() << std::endl;
 		return -1;
