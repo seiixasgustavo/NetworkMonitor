f6275d994b02a412914609fc273cfa319b1e3ecb
Gustavo Nabak Batista Seixas <gunabakbs@gmail.com>
2021-03-06 17:09:55 -0300

    Add code for the Boost Asio crash course

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,51 @@
 #include <boost/asio.hpp>
+#include <boost/asio/strand.hpp>
 #include <boost/system/error_code.hpp>
 
+#include <iomanip>
 #include <iostream>
+#include <thread>
 
 using tcp = boost::asio::ip::tcp;
 
 void Log(boost::system::error_code ec) {
-	std::cerr << (ec ? "Error: " : "OK")
-	  	  << (ec ? ec.message() : "")
-		  << std::endl;
+  std::cerr << "[" << std::setw(14) << std::this_thread::get_id() << "] "
+            << (ec ? "Error: " : "OK") << (ec ? ec.message() : "") << std::endl;
 }
 
+void OnConnect(boost::system::error_code ec) { Log(ec); }
+
 int main() {
-	boost::asio::io_context ioc{};
+  std::cerr << "[" << std::setw(14) << std::this_thread::get_id() << "] main"
+            << std::endl;
+  boost::asio::io_context ioc{};
 
-	tcp::socket socket{ioc};
+  tcp::socket socket{boost::asio::make_strand(ioc)};
 
-	boost::system::error_code ec {};
+  size_t nThreads{4};
 
-	auto address {boost::asio::ip::address::from_string("1.1.1.1")};
-	tcp::endpoint endpoint{address, 80};
-	socket.connect(endpoint, ec);
-	if (ec) {
-		std::cerr << "Error: " << ec.message() << std::endl;
-		return -1;
-	} else {
-		std::cout << "OK" << std::endl;
-		return 0;
-	}
+  boost::system::error_code ec{};
+  tcp::resolver resolver{ioc};
+
+  auto endpoint{resolver.resolve("google.com", "80", ec)};
+  if (ec) {
+    Log(ec);
+    return -1;
+  }
+
+  for (size_t idx{0}; idx < nThreads; ++idx) {
+    socket.async_connect(*endpoint, OnConnect);
+  }
+
+  std::vector<std::thread> threads{};
+  threads.reserve(nThreads);
+
+  for (size_t idx{0}; idx < nThreads; ++idx) {
+    threads.emplace_back([&ioc]() { ioc.run(); });
+  }
+  for (size_t idx{0}; idx < nThreads; ++idx) {
+    threads[idx].join();
+  }
+
+  return 0;
 }
