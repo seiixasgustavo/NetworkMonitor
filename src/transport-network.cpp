8107b192e1f4e3584d2b246e67a0842071034ec2
Gustavo Nabak Batista Seixas <gunabakbs@gmail.com>
2021-04-07 01:05:36 -0300

    Add Transport Network Class

--- src/transport-network.cpp
+++ src/transport-network.cpp
@@ -1,24 +1,377 @@
 #include <NetworkMonitor/transport-network.h>
+
 #include <algorithm>
 #include <memory>
+#include <stdexcept>
 #include <vector>
 
+using NetworkMonitor::Id;
+using NetworkMonitor::Line;
+using NetworkMonitor::PassengerEvent;
+using NetworkMonitor::Route;
+using NetworkMonitor::Station;
 using NetworkMonitor::TransportNetwork;
 
-std::shared_ptr<TransportNetwork::GraphNode>
-TransportNetwork::GetStation(const Id &id) const {
-  auto stationIt{stations_.find(id)};
-  return (stationIt == stations_.end()) ? nullptr : stationIt->second;
+// Station — Public methods
+
+bool Station::operator==(const Station &other) const
+{
+    return id == other.id;
 }
 
-bool TransportNetwork::AddStation(const Station &station) {
-  if (GetStation(station.id) != nullptr) {
-    return false;
-  }
+// Route — Public methods
 
-  auto node{
-      std::make_shared<GraphNode>(GraphNode{station.id, station.name, 0, {}})};
-  stations_.emplace(station.id, std::move(node));
+bool Route::operator==(const Route &other) const
+{
+    return id == other.id;
+}
 
-  return true;
+// Line — Public methods
+
+bool Line::operator==(const Line &other) const
+{
+    return id == other.id;
+}
+
+// TransportNetwork — Public methods
+
+TransportNetwork::TransportNetwork() = default;
+
+TransportNetwork::~TransportNetwork() = default;
+
+TransportNetwork::TransportNetwork(
+    const TransportNetwork &copied) = default;
+
+TransportNetwork::TransportNetwork(
+    TransportNetwork &&moved) = default;
+
+TransportNetwork &TransportNetwork::operator=(
+    const TransportNetwork &copied) = default;
+
+TransportNetwork &TransportNetwork::operator=(
+    TransportNetwork &&moved) = default;
+
+bool TransportNetwork::AddStation(
+    const Station &station)
+{
+    if (GetStation(station.id) != nullptr)
+    {
+        return false;
+    }
+
+    auto node{std::make_shared<GraphNode>(GraphNode{
+        station.id,
+        station.name,
+        0,
+        {},
+    })};
+
+    stations_.emplace(station.id, std::move(node));
+
+    return true;
+}
+
+bool TransportNetwork::AddLine(
+    const Line &line)
+{
+    if (GetLine(line.id) != nullptr)
+    {
+        return false;
+    }
+
+    auto lineInternal{std::make_shared<LineInternal>(LineInternal{
+        line.id,
+        line.name,
+        {},
+    })};
+
+    for (const auto &route : line.routes)
+    {
+        bool ok{AddRouteToLine(route, lineInternal)};
+        if (!ok)
+        {
+            return false;
+        }
+    }
+
+    lines_.emplace(line.id, std::move(lineInternal));
+
+    return true;
+}
+
+bool TransportNetwork::RecordPassengerEvent(
+    const PassengerEvent &event)
+{
+    const auto stationNode{GetStation(event.stationId)};
+    if (stationNode == nullptr)
+    {
+        return false;
+    }
+
+    switch (event.type)
+    {
+    case PassengerEvent::Type::In:
+        stationNode->passengerCount++;
+        return true;
+    case PassengerEvent::Type::Out:
+        stationNode->passengerCount--;
+        return true;
+    default:
+        return false;
+    }
+}
+
+long long int TransportNetwork::GetPassengerCount(
+    const Id &station) const
+{
+    const auto stationNode{GetStation(station)};
+    if (stationNode == nullptr)
+    {
+        throw std::runtime_error("Could not find station in the network: " +
+                                 station);
+    }
+    return stationNode->passengerCount;
+}
+
+std::vector<Id> TransportNetwork::GetRoutesServingStation(
+    const Id &station) const
+{
+    const auto stationNode{GetStation(station)};
+    if (stationNode == nullptr)
+    {
+        return {};
+    }
+
+    std::vector<Id> routes{};
+    const auto &edges{stationNode->edges};
+    for (const auto &edge : edges)
+    {
+        routes.push_back(edge->route->id);
+    }
+
+    // The previous loop misses a corner case: The end station of a route does
+    // not have any edge containing that route, because we only track the routes
+    // that *leave from*, not *arrive to* a certain station.
+    // We need to loop over all line routes to check if our station is the end
+    // stop of any route.
+    // FIXME: In the worst case, we are iterating over all routes for all
+    //        lines in the network. We may want to optimize this.
+    for (const auto &[_, line] : lines_)
+    {
+        for (const auto &[_, route] : line->routes)
+        {
+            const auto &endStop{route->stops[route->stops.size() - 1]};
+            if (stationNode == endStop)
+            {
+                routes.push_back(route->id);
+            }
+        }
+    }
+
+    return routes;
+}
+
+bool TransportNetwork::SetTravelTime(
+    const Id &stationA,
+    const Id &stationB,
+    const unsigned int travelTime)
+{
+    const auto stationANode{GetStation(stationA)};
+    const auto stationBNode{GetStation(stationB)};
+    if (stationANode == nullptr || stationBNode == nullptr)
+    {
+        return false;
+    }
+
+    bool foundAnyEdge{false};
+    auto setTravelTime{[&foundAnyEdge, &travelTime](auto from, auto to) {
+        for (auto &edge : from->edges)
+        {
+            if (edge->nextStop == to)
+            {
+                edge->travelTime = travelTime;
+                foundAnyEdge = true;
+            }
+        }
+    }};
+
+    setTravelTime(stationANode, stationBNode);
+    setTravelTime(stationBNode, stationANode);
+
+    return foundAnyEdge;
+}
+
+unsigned int TransportNetwork::GetTravelTime(
+    const Id &stationA,
+    const Id &stationB) const
+{
+    const auto stationANode{GetStation(stationA)};
+    const auto stationBNode{GetStation(stationB)};
+    if (stationANode == nullptr || stationBNode == nullptr)
+    {
+        return 0;
+    }
+
+    for (const auto &edge : stationANode->edges)
+    {
+        if (edge->nextStop == stationBNode)
+        {
+            return edge->travelTime;
+        }
+    }
+
+    for (const auto &edge : stationBNode->edges)
+    {
+        if (edge->nextStop == stationANode)
+        {
+            return edge->travelTime;
+        }
+    }
+
+    return 0;
+}
+
+unsigned int TransportNetwork::GetTravelTime(
+    const Id &line,
+    const Id &route,
+    const Id &stationA,
+    const Id &stationB) const
+{
+    const auto routeInternal{GetRoute(line, route)};
+    if (routeInternal == nullptr)
+    {
+        return 0;
+    }
+
+    const auto stationANode{GetStation(stationA)};
+    const auto stationBNode{GetStation(stationB)};
+    if (stationANode == nullptr || stationBNode == nullptr)
+    {
+        return 0;
+    }
+
+    unsigned int travelTime{0};
+    bool foundA{false};
+
+    for (const auto &stop : routeInternal->stops)
+    {
+        if (stop == stationANode)
+        {
+            foundA = true;
+        }
+
+        if (stop == stationBNode)
+        {
+            return travelTime;
+        }
+
+        if (foundA)
+        {
+            auto edgeIt{stop->FindEdgeForRoute(routeInternal)};
+            if (edgeIt == stop->edges.end())
+            {
+                return 0;
+            }
+            travelTime += (*edgeIt)->travelTime;
+        }
+    }
+
+    return 0;
+}
+
+// TransportNetwork — Private methods
+
+std::vector<
+    std::shared_ptr<TransportNetwork::GraphEdge>>::const_iterator
+TransportNetwork::GraphNode::FindEdgeForRoute(
+    const std::shared_ptr<RouteInternal> &route) const
+{
+    return std::find_if(
+        edges.begin(),
+        edges.end(),
+        [&route](const auto &edge) {
+            return edge->route == route;
+        });
+}
+
+std::shared_ptr<TransportNetwork::GraphNode> TransportNetwork::GetStation(
+    const Id &stationId) const
+{
+    auto stationIt{stations_.find(stationId)};
+    if (stationIt == stations_.end())
+    {
+        return nullptr;
+    }
+    return stationIt->second;
+}
+
+std::shared_ptr<TransportNetwork::LineInternal> TransportNetwork::GetLine(
+    const Id &lineId) const
+{
+    auto lineIt{lines_.find(lineId)};
+    if (lineIt == lines_.end())
+    {
+        return nullptr;
+    }
+    return lineIt->second;
+}
+
+std::shared_ptr<TransportNetwork::RouteInternal> TransportNetwork::GetRoute(
+    const Id &lineId,
+    const Id &routeId) const
+{
+    auto line{GetLine(lineId)};
+    if (line == nullptr)
+    {
+        return nullptr;
+    }
+    const auto &routes{line->routes};
+    auto routeIt{routes.find(routeId)};
+    if (routeIt == routes.end())
+    {
+        return nullptr;
+    }
+    return routeIt->second;
+}
+
+bool TransportNetwork::AddRouteToLine(
+    const Route &route,
+    const std::shared_ptr<LineInternal> &lineInternal)
+{
+    if (lineInternal->routes.find(route.id) != lineInternal->routes.end())
+    {
+        return false;
+    }
+
+    std::vector<std::shared_ptr<GraphNode>> stops{};
+    stops.reserve(route.stops.size());
+    for (const auto &stopId : route.stops)
+    {
+        const auto station{GetStation(stopId)};
+        if (station == nullptr)
+        {
+            return false;
+        }
+        stops.push_back(station);
+    }
+
+    auto routeInternal{std::make_shared<RouteInternal>(RouteInternal{
+        route.id,
+        lineInternal,
+        std::move(stops)})};
+
+    for (size_t idx{0}; idx < routeInternal->stops.size() - 1; ++idx)
+    {
+        const auto &thisStop{routeInternal->stops[idx]};
+        const auto &nextStop{routeInternal->stops[idx + 1]};
+        thisStop->edges.emplace_back(std::make_shared<GraphEdge>(GraphEdge{
+            routeInternal,
+            nextStop,
+            0,
+        }));
+    }
+
+    lineInternal->routes[route.id] = std::move(routeInternal);
+
+    return true;
 }
--- tests/websocket-client.cpp
+++ tests/websocket-client.cpp
@@ -9,70 +9,155 @@
 
 using NetworkMonitor::WebSocketClient;
 
-BOOST_AUTO_TEST_SUITE(NetworkMonitor);
+BOOST_AUTO_TEST_SUITE(network_monitor);
 
-BOOST_AUTO_TEST_CASE(cacert_pem) {
-  BOOST_CHECK(std::filesystem::exists(TESTS_CACERT_PEM));
+BOOST_AUTO_TEST_CASE(cacert_pem)
+{
+    BOOST_CHECK(std::filesystem::exists(TESTS_CACERT_PEM));
 }
 
-BOOST_AUTO_TEST_CASE(class_WebSocketClient) {
-  const std::string url{"echo.wss-websocket.net"};
-  const std::string endpoint{"/network-events"};
-  const std::string port{"443"};
+BOOST_AUTO_TEST_CASE(class_WebSocketClient)
+{
+    // Connection targets
+    const std::string url{"echo.websocket.org"};
+    const std::string endpoint{"/"};
+    const std::string port{"443"};
+    const std::string message{"Hello WebSocket"};
 
-  const std::string username{"fake_username"};
-  const std::string password{"fake_password"};
+    // TLS context
+    boost::asio::ssl::context ctx{boost::asio::ssl::context::tlsv12_client};
+    ctx.load_verify_file(TESTS_CACERT_PEM);
 
-  std::stringstream ss{};
-  ss << "STOMP" << std::endl
-     << "accept-version:1.2" << std::endl
-     << "host:transportforlondon.com" << std::endl
-     << "login:" << username << std::endl
-     << "passcode:" << password << std::endl
-     << std::endl
-     << '\0';
+    // Always start with an I/O context object.
+    boost::asio::io_context ioc{};
 
-  const std::string message{ss.str()};
+    // The class under test
+    WebSocketClient client{url, endpoint, port, ioc, ctx};
 
-  boost::asio::ssl::context ctx{boost::asio::ssl::context::tlsv12_client};
-  ctx.load_verify_file(TESTS_CACERT_PEM);
+    // We use these flags to check that the connection, send, receive functions
+    // work as expected.
+    bool connected{false};
+    bool messageSent{false};
+    bool messageReceived{false};
+    bool disconnected{false};
+    std::string echo{};
 
-  boost::asio::io_context ioc{};
+    // Our own callbacks
+    auto onSend{[&messageSent](auto ec) {
+        messageSent = !ec;
+    }};
+    auto onConnect{[&client, &connected, &onSend, &message](auto ec) {
+        connected = !ec;
+        if (!ec)
+        {
+            client.Send(message, onSend);
+        }
+    }};
+    auto onClose{[&disconnected](auto ec) {
+        disconnected = !ec;
+    }};
+    auto onReceive{[&client,
+                    &onClose,
+                    &messageReceived,
+                    &echo](auto ec, auto received) {
+        messageReceived = !ec;
+        echo = std::move(received);
+        client.Close(onClose);
+    }};
 
-  WebSocketClient client{url, endpoint, port, ioc, ctx};
+    // We must call io_context::run for asynchronous callbacks to run.
+    client.Connect(onConnect, onReceive);
+    ioc.run();
 
-  bool connected{false};
-  bool messageSent{false};
-  bool messageReceived{false};
-  bool disconnected{false};
-  std::string echo;
+    // When we get here, the io_context::run function has run out of work to do.
+    BOOST_CHECK(connected);
+    BOOST_CHECK(messageSent);
+    BOOST_CHECK(messageReceived);
+    BOOST_CHECK(disconnected);
+    BOOST_CHECK_EQUAL(message, echo);
+}
 
-  auto onSend{[&messageSent](auto ec) { messageSent = !ec; }};
+bool CheckResponse(const std::string &response)
+{
+    // We do not parse the whole message. We only check that it contains some
+    // expected items.
+    bool ok{true};
+    ok &= response.find("ERROR") != std::string::npos;
+    ok &= response.find("ValidationInvalidAuth") != std::string::npos;
+    return ok;
+}
 
-  auto onConnect{[&client, &connected, &onSend, &message](auto ec) {
-    connected = !ec;
-    if (!ec) {
-      client.Send(message, onSend);
-    }
-  }};
+BOOST_AUTO_TEST_CASE(send_stomp_frame)
+{
+    // Connection targets
+    const std::string url{"ltnm.learncppthroughprojects.com"};
+    const std::string endpoint{"/network-events"};
+    const std::string port{"443"};
 
-  auto onClose{[&disconnected](auto ec) { disconnected = !ec; }};
+    // STOMP frame
+    const std::string username{"fake_username"};
+    const std::string password{"fake_password"};
+    std::stringstream ss{};
+    ss << "STOMP" << std::endl
+       << "accept-version:1.2" << std::endl
+       << "host:transportforlondon.com" << std::endl
+       << "login:" << username << std::endl
+       << "passcode:" << password << std::endl
+       << std::endl // Headers need to be followed by a blank line.
+       << '\0';     // The body (even if absent) must be followed by a NULL octet.
+    const std::string message{ss.str()};
 
-  auto onReceive{[&client, &onClose, &messageReceived, &echo,
-                  &message](auto ec, auto received) {
-    messageReceived = !ec;
-    echo = received;
-    client.Close(onClose);
-  }};
+    // TLS context
+    boost::asio::ssl::context ctx{boost::asio::ssl::context::tlsv12_client};
+    ctx.load_verify_file(TESTS_CACERT_PEM);
 
-  client.Connect(onConnect, onReceive);
-  ioc.run();
+    // Always start with an I/O context object.
+    boost::asio::io_context ioc{};
 
-  BOOST_CHECK(connected);
-  BOOST_CHECK(messageSent);
-  BOOST_CHECK(messageReceived);
-  BOOST_CHECK(disconnected);
-  BOOST_CHECK_EQUAL(message, echo);
+    // The class under test
+    WebSocketClient client{url, endpoint, port, ioc, ctx};
+
+    // We use these flags to check that the connection, send, receive functions
+    // work as expected.
+    bool connected{false};
+    bool messageSent{false};
+    bool messageReceived{false};
+    bool disconnected{false};
+    std::string response{};
+
+    // Our own callbacks
+    auto onSend{[&messageSent](auto ec) {
+        messageSent = !ec;
+    }};
+    auto onConnect{[&client, &connected, &onSend, &message](auto ec) {
+        connected = !ec;
+        if (!ec)
+        {
+            client.Send(message, onSend);
+        }
+    }};
+    auto onClose{[&disconnected](auto ec) {
+        disconnected = !ec;
+    }};
+    auto onReceive{[&client,
+                    &onClose,
+                    &messageReceived,
+                    &response](auto ec, auto received) {
+        messageReceived = !ec;
+        response = std::move(received);
+        client.Close(onClose);
+    }};
+
+    // We must call io_context::run for asynchronous callbacks to run.
+    client.Connect(onConnect, onReceive);
+    ioc.run();
+
+    // When we get here, the io_context::run function has run out of work to do.
+    BOOST_CHECK(connected);
+    BOOST_CHECK(messageSent);
+    BOOST_CHECK(messageReceived);
+    BOOST_CHECK(disconnected);
+    BOOST_CHECK(CheckResponse(response));
 }
 
 BOOST_AUTO_TEST_SUITE_END();
